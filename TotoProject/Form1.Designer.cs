08fd699063bae772eda99c649cf58ca85e8e2e97
Nikola Valkov <bulgarianmapper64@gmail.com>
2022-12-03 13:11:23 +0200

    Interface

--- TotoProject/Form1.Designer.cs
+++ TotoProject/Form1.Designer.cs
@@ -28,13 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rb6ot49 = new System.Windows.Forms.RadioButton();
+            this.rb6ot42 = new System.Windows.Forms.RadioButton();
+            this.rb5ot35 = new System.Windows.Forms.RadioButton();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.yellowCircle = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPurvoChislo = new System.Windows.Forms.Label();
+            this.lblVtoroChislo = new System.Windows.Forms.Label();
+            this.lblTretoChislo = new System.Windows.Forms.Label();
+            this.lblShestoChislo = new System.Windows.Forms.Label();
+            this.lblPetoChislo = new System.Windows.Forms.Label();
+            this.lblChetvurtoChislo = new System.Windows.Forms.Label();
+            this.txtboxPurvoChislo = new System.Windows.Forms.TextBox();
+            this.txtboxVtoroChislo = new System.Windows.Forms.TextBox();
+            this.txtboxTretoChislo = new System.Windows.Forms.TextBox();
+            this.txtboxShestoChislo = new System.Windows.Forms.TextBox();
+            this.txtboxPetoChislo = new System.Windows.Forms.TextBox();
+            this.txtboxChetvurtoChislo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowCircle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rb6ot49
+            // 
+            this.rb6ot49.AutoSize = true;
+            this.rb6ot49.Location = new System.Drawing.Point(64, 106);
+            this.rb6ot49.Name = "rb6ot49";
+            this.rb6ot49.Size = new System.Drawing.Size(14, 13);
+            this.rb6ot49.TabIndex = 0;
+            this.rb6ot49.TabStop = true;
+            this.rb6ot49.UseVisualStyleBackColor = true;
+            // 
+            // rb6ot42
+            // 
+            this.rb6ot42.AutoSize = true;
+            this.rb6ot42.Location = new System.Drawing.Point(218, 106);
+            this.rb6ot42.Name = "rb6ot42";
+            this.rb6ot42.Size = new System.Drawing.Size(14, 13);
+            this.rb6ot42.TabIndex = 1;
+            this.rb6ot42.TabStop = true;
+            this.rb6ot42.UseVisualStyleBackColor = true;
+            // 
+            // rb5ot35
+            // 
+            this.rb5ot35.AutoSize = true;
+            this.rb5ot35.Location = new System.Drawing.Point(363, 106);
+            this.rb5ot35.Name = "rb5ot35";
+            this.rb5ot35.Size = new System.Drawing.Size(14, 13);
+            this.rb5ot35.TabIndex = 2;
+            this.rb5ot35.TabStop = true;
+            this.rb5ot35.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::TotoProject.Properties.Resources.redCircle;
+            this.pictureBox2.Location = new System.Drawing.Point(322, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 71);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::TotoProject.Properties.Resources.blueCircle;
+            this.pictureBox1.Location = new System.Drawing.Point(177, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 72);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // yellowCircle
+            // 
+            this.yellowCircle.BackColor = System.Drawing.Color.Transparent;
+            this.yellowCircle.Image = global::TotoProject.Properties.Resources.yellowCircle;
+            this.yellowCircle.Location = new System.Drawing.Point(24, 28);
+            this.yellowCircle.Name = "yellowCircle";
+            this.yellowCircle.Size = new System.Drawing.Size(100, 72);
+            this.yellowCircle.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.yellowCircle.TabIndex = 3;
+            this.yellowCircle.TabStop = false;
+            this.yellowCircle.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(58, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "6/49";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(213, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "6/42";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(357, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "5/35";
+            // 
+            // lblPurvoChislo
+            // 
+            this.lblPurvoChislo.AutoSize = true;
+            this.lblPurvoChislo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPurvoChislo.Location = new System.Drawing.Point(24, 160);
+            this.lblPurvoChislo.Name = "lblPurvoChislo";
+            this.lblPurvoChislo.Size = new System.Drawing.Size(119, 20);
+            this.lblPurvoChislo.TabIndex = 9;
+            this.lblPurvoChislo.Text = "Първо число";
+            // 
+            // lblVtoroChislo
+            // 
+            this.lblVtoroChislo.AutoSize = true;
+            this.lblVtoroChislo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblVtoroChislo.Location = new System.Drawing.Point(24, 214);
+            this.lblVtoroChislo.Name = "lblVtoroChislo";
+            this.lblVtoroChislo.Size = new System.Drawing.Size(116, 20);
+            this.lblVtoroChislo.TabIndex = 10;
+            this.lblVtoroChislo.Text = "Второ число";
+            // 
+            // lblTretoChislo
+            // 
+            this.lblTretoChislo.AutoSize = true;
+            this.lblTretoChislo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTretoChislo.Location = new System.Drawing.Point(24, 264);
+            this.lblTretoChislo.Name = "lblTretoChislo";
+            this.lblTretoChislo.Size = new System.Drawing.Size(114, 20);
+            this.lblTretoChislo.TabIndex = 11;
+            this.lblTretoChislo.Text = "Трето число";
+            // 
+            // lblShestoChislo
+            // 
+            this.lblShestoChislo.AutoSize = true;
+            this.lblShestoChislo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblShestoChislo.Location = new System.Drawing.Point(243, 264);
+            this.lblShestoChislo.Name = "lblShestoChislo";
+            this.lblShestoChislo.Size = new System.Drawing.Size(117, 20);
+            this.lblShestoChislo.TabIndex = 14;
+            this.lblShestoChislo.Text = "Шесто число";
+            // 
+            // lblPetoChislo
+            // 
+            this.lblPetoChislo.AutoSize = true;
+            this.lblPetoChislo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPetoChislo.Location = new System.Drawing.Point(243, 214);
+            this.lblPetoChislo.Name = "lblPetoChislo";
+            this.lblPetoChislo.Size = new System.Drawing.Size(107, 20);
+            this.lblPetoChislo.TabIndex = 13;
+            this.lblPetoChislo.Text = "Пето число";
+            // 
+            // lblChetvurtoChislo
+            // 
+            this.lblChetvurtoChislo.AutoSize = true;
+            this.lblChetvurtoChislo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblChetvurtoChislo.Location = new System.Drawing.Point(243, 160);
+            this.lblChetvurtoChislo.Name = "lblChetvurtoChislo";
+            this.lblChetvurtoChislo.Size = new System.Drawing.Size(149, 20);
+            this.lblChetvurtoChislo.TabIndex = 12;
+            this.lblChetvurtoChislo.Text = "Четвърто число";
+            // 
+            // txtboxPurvoChislo
+            // 
+            this.txtboxPurvoChislo.Location = new System.Drawing.Point(149, 160);
+            this.txtboxPurvoChislo.Name = "txtboxPurvoChislo";
+            this.txtboxPurvoChislo.Size = new System.Drawing.Size(27, 20);
+            this.txtboxPurvoChislo.TabIndex = 15;
+            // 
+            // txtboxVtoroChislo
+            // 
+            this.txtboxVtoroChislo.Location = new System.Drawing.Point(149, 216);
+            this.txtboxVtoroChislo.Name = "txtboxVtoroChislo";
+            this.txtboxVtoroChislo.Size = new System.Drawing.Size(27, 20);
+            this.txtboxVtoroChislo.TabIndex = 16;
+            // 
+            // txtboxTretoChislo
+            // 
+            this.txtboxTretoChislo.Location = new System.Drawing.Point(149, 266);
+            this.txtboxTretoChislo.Name = "txtboxTretoChislo";
+            this.txtboxTretoChislo.Size = new System.Drawing.Size(27, 20);
+            this.txtboxTretoChislo.TabIndex = 17;
+            // 
+            // txtboxShestoChislo
+            // 
+            this.txtboxShestoChislo.Location = new System.Drawing.Point(404, 266);
+            this.txtboxShestoChislo.Name = "txtboxShestoChislo";
+            this.txtboxShestoChislo.Size = new System.Drawing.Size(27, 20);
+            this.txtboxShestoChislo.TabIndex = 20;
+            // 
+            // txtboxPetoChislo
+            // 
+            this.txtboxPetoChislo.Location = new System.Drawing.Point(404, 216);
+            this.txtboxPetoChislo.Name = "txtboxPetoChislo";
+            this.txtboxPetoChislo.Size = new System.Drawing.Size(27, 20);
+            this.txtboxPetoChislo.TabIndex = 19;
+            // 
+            // txtboxChetvurtoChislo
+            // 
+            this.txtboxChetvurtoChislo.Location = new System.Drawing.Point(404, 160);
+            this.txtboxChetvurtoChislo.Name = "txtboxChetvurtoChislo";
+            this.txtboxChetvurtoChislo.Size = new System.Drawing.Size(27, 20);
+            this.txtboxChetvurtoChislo.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::TotoProject.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(452, 336);
+            this.Controls.Add(this.txtboxShestoChislo);
+            this.Controls.Add(this.txtboxPetoChislo);
+            this.Controls.Add(this.txtboxChetvurtoChislo);
+            this.Controls.Add(this.txtboxTretoChislo);
+            this.Controls.Add(this.txtboxVtoroChislo);
+            this.Controls.Add(this.txtboxPurvoChislo);
+            this.Controls.Add(this.lblShestoChislo);
+            this.Controls.Add(this.lblPetoChislo);
+            this.Controls.Add(this.lblChetvurtoChislo);
+            this.Controls.Add(this.lblTretoChislo);
+            this.Controls.Add(this.lblVtoroChislo);
+            this.Controls.Add(this.lblPurvoChislo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.yellowCircle);
+            this.Controls.Add(this.rb5ot35);
+            this.Controls.Add(this.rb6ot42);
+            this.Controls.Add(this.rb6ot49);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yellowCircle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rb6ot49;
+        private System.Windows.Forms.RadioButton rb6ot42;
+        private System.Windows.Forms.RadioButton rb5ot35;
+        private System.Windows.Forms.PictureBox yellowCircle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPurvoChislo;
+        private System.Windows.Forms.Label lblVtoroChislo;
+        private System.Windows.Forms.Label lblTretoChislo;
+        private System.Windows.Forms.Label lblShestoChislo;
+        private System.Windows.Forms.Label lblPetoChislo;
+        private System.Windows.Forms.Label lblChetvurtoChislo;
+        private System.Windows.Forms.TextBox txtboxPurvoChislo;
+        private System.Windows.Forms.TextBox txtboxVtoroChislo;
+        private System.Windows.Forms.TextBox txtboxTretoChislo;
+        private System.Windows.Forms.TextBox txtboxShestoChislo;
+        private System.Windows.Forms.TextBox txtboxPetoChislo;
+        private System.Windows.Forms.TextBox txtboxChetvurtoChislo;
     }
 }
 
